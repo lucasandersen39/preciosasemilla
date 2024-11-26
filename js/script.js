a8705ff1a630c13d14efd85c2bfa6c38c0dfc88f
Vanina Pozzobon <vanpozzobon@gmail.com>
2024-11-26 19:09:08 -0300

    redireccion y cuadro de dialogo en boton dona

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ function toggleMenu() {
 
 }
 
+const botones=document.querySelectorAll('.btn-redirigir');
+
+botones.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+        // Mostrar el cuadro de diálogo
+        const dialogo = document.getElementById('dialogo');
+        const mensajeDialogo = document.getElementById('mensaje-dialogo');
+        
+        // Cambiar el mensaje del diálogo
+        mensajeDialogo.textContent = 'Redirigiendo al sitio, aguarde...';
+        
+        // Hacer visible el cuadro
+        dialogo.classList.add('dialogo-visible');
+
+        // Redirigir después de 3 segundos
+        setTimeout(() => {
+            window.location.href = 'https://www.mercadopago.com'; // Cambia a tu URL
+        }, 3000); // 3 segundos de espera
+    });
+});
+
+
+
